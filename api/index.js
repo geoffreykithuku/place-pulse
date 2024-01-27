379d518b9ce8d4470ddb15b96603fae8a15785ab
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-01-27 23:08:26 +0300

    connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello")
-})
+
+
+// mongo db connection
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("connected to mongoDB"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 app.listen(3000, () => {
-    console.log("app running on port 3000")
-})
+  console.log("app running on port 3000");
+});
