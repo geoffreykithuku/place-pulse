c58aa37efad5a09bef3b0ac8c8f7712cbdeafdca
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-01-28 21:47:13 +0300

    signin api routes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   // destructure user params from request.body
@@ -15,6 +16,40 @@ export const signup = async (req, res, next) => {
     });
 
     res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    // find user by email
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return res.status(500).json({ message: "User not found" });
+    }
+
+    // compare user password with hashed password
+    const validPassword = await bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+
+    if (!validPassword) {
+      return res.status(500).json({ message: "Invalid password" });
+    }
+
+    // create token 
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const { password: pass, ...rest } = validUser._doc;
+    
+    // create cookie with token
+    res.cookie("token", token, { httpOnly: true }).status(200).json(rest);
+
   } catch (error) {
     next(error);
   }
